32cbaea4c006df8aee64f1f4eb2111c886dfb7bb
Thor Jorud <thorjorud06@gmail.com>
2026-07-21 17:11:47 -0500

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        '''
+    
+        prev = None
+        curr = head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        return prev
+
+    '''
+    Brute Force Approach: We could first create a copy of the linked list 
+    using a stack. Then iterate through the linked list again rewriting the
+    nodes from the end of the stack.
+        - Time Complexity: O(n)
+        - Space Complexity: O(n)
+    '''
