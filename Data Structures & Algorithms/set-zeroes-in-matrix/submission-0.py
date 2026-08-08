02dc731f29345187a56a016d6960747952013dec
Thor Jorud <thorjorud06@gmail.com>
2026-08-08 12:18:42 -0500

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows, cols = len(matrix), len(matrix[0])
+        row_zero = False # Tracks if 0th row contains zeros.
+
+        # Set up flags for where 0's should go in row/col headers.
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0 # Mark the column header.
+                    if r > 0:
+                        matrix[r][0] = 0 # Mark the row header.
+                    else:
+                        row_zero = True # Top row has a zero!
+
+        # Zero out the grid.
+        # Skip the top row and left col since they hold our marks.
+        for r in range(1, rows):
+            for c in range(1, cols):
+                # Check if row/col header are zero.
+                # If they are set element to zero.
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        
+        # Check if col 0 was flagged.
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
+
+        # Check if row 0 was flagged.
+        if row_zero:
+            for c in range(cols):
+                matrix[0][c] = 0
