d3a47826e017d394dcf6e7a290b624212869c10a
Thor Jorud <thorjorud06@gmail.com>
2026-08-24 13:42:01 -0500

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    '''
+    Time Complexity: O(m * n)
+        - n = # of nodes in root tree.
+        - m = # of nodes in subRoot tree
+    Space Complexity: O(h) or O(n + m)
+        - Determined by the maxiumum depth of the recursion call stack.
+        - If trees are balanced, the height is logarithmic O(log n + log m) space.
+        - If trees are skewed we get O(n + m)
+    '''
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        
+        if not p or not q or p.val != q.val:
+            return False
+        
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        
+        if self.isSameTree(root, subRoot):
+            return True
+        
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
