e902da8224eb4f533c07dcf37a146ce41a379f6d
Thor Jorud <thorjorud06@gmail.com>
2026-08-25 17:02:22 -0500

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    '''
+    Time: O(n)
+        - Each char can be hit a max of 2 times.
+    Space: O(m)
+        - The seen set can hold up to m unique chars from s.
+    '''
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_win = 0
+        seen = set()
+
+        left = 0
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            max_win = max(max_win, right - left + 1)
+        return max_win
+
+
+        
+            
