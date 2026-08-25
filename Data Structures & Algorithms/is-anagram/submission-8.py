d18d40e4c4fb916e6079d30a1e286424d2e141b1
Thor Jorud <thorjorud06@gmail.com>
2026-08-25 13:04:59 -0500

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # If length of t and s are not the same, they cannot be anagrams.
+        if len(s) != len(t):
+            return False
+
+        s_map = {}
+
+        # Buid freq map for s.
+        for c in s:
+            s_map[c] = s_map.get(c, 0) + 1
+
+        for c in t:
+            if c not in s_map or s_map[c] == 0:
+                return False
+
+            s_map[c] -= 1
+
+        return True
+
+        
+
+        
